7bb8abdec22abadceb033128fe921933e2188e0c
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-09-28 15:15:26 +0800

    update route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const controllers = require('../controllers');
 const router = express.Router();
+const controllers = require('../controllers');
+const middleware = require('../helper/middleware');
 
 router.post('/auth/register', controllers.register);
 router.post('/auth/login', controllers.login);
-router.post('/user/details', controllers.login);
+router.get('/user/details', middleware.mustLogin, controllers.userDetails); 
 
 module.exports = router
 
