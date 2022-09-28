1209edf3af3c9ab9f4aae435d333f0c86d4a212a
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-09-28 16:21:46 +0800

    add route for update password

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const middleware = require('../helper/middleware');
 
 router.post('/auth/register', controllers.register);
 router.post('/auth/login', controllers.login);
+router.put('/auth/update-password', middleware.mustLogin, controllers.updatePassword); 
 router.get('/user/details', middleware.mustLogin, controllers.userDetails); 
 
 module.exports = router
